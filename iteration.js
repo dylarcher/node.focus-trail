49182265e74f161ec8c632ea5661f1608a5931ce
Dylan Archer <dylarcher@gmail.com>
2021-10-21 14:54:05 -0400

    Update iteration.js

--- iteration.js
+++ iteration.js
@@ -1,47 +1,77 @@
-class Iterate {
+import Helper from '/helpers.js'
 
-  mapArr([head, ...tail], fn) {
-  	return !head && !tail.length ? [] : [
-      ...this.mapArr(tail, fn),
-    	fn(head)
+class Iterator {
+  map(obj, fn) {
+  	let head, tail
+    // when object
+  	if (Helper.isObject(obj)) {
+    	// namespace head with first keyname
+      head = Object.keys(obj)[0]
+      // tail contains remaining entries, without head
+      const { [head]: value, ...tail } = obj
+      // check head & tail value status, return object
+      return !head && !Object
+        .values(tail)
+        .length ? {} : {
+        	// recursively rerun remaining entries
+          ...this.map(tail, fn),
+          // pass head value to callback
+          [head]: fn(value, head)
+        }
+    }
+    // when not object (e.g. map, set, array, or string)
+    // namespace 0 index to head, remaining to tail
+    [head, ...tail] = obj
+    // check head & tail status, return array
+    return !head && !tail.length ? [] : [
+    	// recursively rerun remaining entries
+      ...this.map(tail, fn),
+      // pass head to callback
+      fn(head)
     ]
   }
-  
-  mapObj(obj, fn) {
-    const head = Object.keys(obj)[0]
-  	const { [head]: value, ...tail } = obj
-    return !head && !Object
-      .values(tail)
-      .length ? {} : {
-      ...this.mapObj(tail, fn),
-      [head]: fn(value, head)
+  filter(obj, fn) {
+  	let head, tail
+    // when object
+  	if (Helper.isObject(obj)) {
+    	// namespace head with first keyname
+      head = Object.keys(obj)[0]
+      // tail contains remaining entries, without head
+      const { [head]: value, ...tail } = obj
+      // check head & tail value status
+      return !head ? {} : fn(value, head)
+      	// recursively rerun remaining entries & merge with head entry
+        ? { [head]: value, ...this.filter(tail, fn) }
+      	// recursively rerun remaining entries
+        : { ...this.filter(tail, fn) }
     }
+    // when not object (e.g. map, set, array, or string)
+    // namespace 0 index to head, remaining to tail
+    [head, ...tail] = obj
+    // check head & tail status
+    return !head ? [] : fn(head) ?
+    	// recursively rerun remaining entries & concat with head
+      [head, ...this.filter(tail, fn)] :
+    	// recursively rerun remaining entries
+      [...this.filter(tail, fn)]
   }
-  
-  filterArr([head, ...tail], fn) {
-    return !head ? [] : fn(head)
-      ? [head, ...this.filterArr(tail, fn)]
-      : [...this.filterArr(tail, fn)]
+  reduce(obj, fn, acc = {}) {
+  	let head, tail
+    // when object
+  	if (Helper.isObject(obj)) {
+    	// namespace head with first keyname
+      head = Object.keys(obj)[0]
+      // tail contains remaining entries, without head
+      const { [head]: value, ...tail } = obj
+      // recursively rerun remaining entries, return acc if head is null
+      return !head ? acc : this.reduce(tail, fn, fn(acc, value, head))
+    }
+    // when not object (e.g. map, set, array, or string)
+    // namespace 0 index to head, remaining to tail
+    [head, ...tail] = obj
+    // recursively rerun remaining entries, return acc if head is null
+    return !head ? acc : this.reduce(tail, fn, fn(acc, head))
   }
-  
-  filterObj(obj, fn) {
-    const head = Object.keys(obj)[0]
-  	const { [head]: value, ...tail } = obj
-    return !head ? {} : fn(value, head)
-    	? { [head]: value, ...this.filterObj(tail, fn) }
-      : { ...this.filterObj(tail, fn) }
-  }
-  
-  reduceArr([head, ...tail], fn, acc = 0) {
-  	return !head ? acc : this.reduceArr(tail, fn, fn(acc, head))
-  }
-  
-  reduceObj(obj, fn, acc = {}) {
-    const head = Object.keys(obj)[0]
-  	const { [head]: value, ...tail } = obj
-    return !head ? acc : this.reduceObj(tail, fn, fn(acc, value, head))
-  }
-
 }
 
-export default new Iterate()
+export default new Iterator()
