dd5e8f7d4b32cf6545d2889766ade3c4b96574cf
Dylan Archer <dylarcher@gmail.com>
2021-11-28 08:18:14 -0500

    Update with new FFlow js

--- focus-flow.js
+++ focus-flow.js
@@ -1,100 +1,290 @@
-import options from '/configs.js'
-import Query from '/query.js'
+import '../css/focus-flow.css';
 
 class FocusFlow {
-  constructor({ selector, actionKey, wrapper }) {
-    this.selector = selector
-    // designate action key (e.g. tab keycode is 9)
-    this.actionKey = actionKey
-    this.wrapper = ( // designate wrapper
-      document.querySelector(wrapper) ||
-      document.body
-    )
-    // get node, if able
-    this.node = this
-    	.wrapper
-      .querySelector(selector)
-  }
+	matchRef(value, ref) {
+		return value === ref;
+	}
 
-  mountNode() {
-    // check for # or . to determine selector type
-    const isId = this.selector.includes('#')
-    const isClass = this.selector.includes('.')
-    // remove # or . if present
-    const name = isId || isClass
-      ? this.selector.substring(1)
-      : this.selector
-    // assign name to class or id string
-    const type = isId && `id="${name}"` ||
-      isClass && `class="${name}"` || ''
-    // html template string for tail node
-    const html = `<div ${type} role="presentation" />`
-    // insert tail div, before closing tag of wrapper
-    this.wrapper.insertAdjacentHTML('beforeend', html)
-    // return FocusFlow for chaining
-    return this
-  }
+	isNull(value) {
+		return this.matchRef(null, value);
+	}
 
-  focusFlow(self = this) {
-    return function (event) {
-      // if node is not yet created
-      if (!self.node) {
-      	// mount node
-        self.mountNode()
-        // assign newly placed element
-        self.node = self.wrapper.querySelector(self.selector)
-      }
-      // event type check
-      const eventType = event.type
-      // tail node style object
-			const flowStyle = self.node.style
-      // reusable target variable
-      const target = event.target
-      // click events condition
-			const click = eventType === 'click'
-      // ensure activeElement is a focusable node
-      const match = Query
-      	.allFocusableNodes()
-        .includes(target)
-      // while users typing with tab key
-      const tabPress = (
-      	eventType === 'keyup' &&
-        event.which === self.actionKey
-      )
-      // checks for user input elements
-      const formField = options
-      	.inputNodes
-        .includes(target.localName)
-      // if window resizes or scrolls
-      const windowMoved = [
-      	'resize',
-        'scroll'
-      ].includes(eventType)
-      // on window move or unfocusable node click
-      if (windowMoved || (click && !match)) {
-      	// shrink tail node so it's hidden, return out
-        return Object.assign(flowStyle, { transform: 'scale(0.001)' })
-      }
-      // on tab or click of a focusable element
-      if ((tabPress || click) && match) {
-      	// get user input parent selectors
-      	const inputWrapper = options.form.fieldWrapper
-        // user input area is active while parent selector is avaliable
-        const inputFieldParent = formField && inputWrapper && inputWrapper.length
-      	// if user input area, wrap parentNode
-        return inputFieldParent
-          ? Object.assign(flowStyle, {
-	    // get active node
-	    ...query.activeNodeState(target
-	    // get active node parent
-	    .closest(inputWrapper)
-	    // get active node parent color
-	    ), color: getComputedStyle(target).color
-          }) // if not user input area, only interactable, wrap activeNode
-          : Object.assign(flowStyle, query.activeNodeState())
-      }
-    }
-  }
+	isDefined(value) {
+		return this.matchRef(undefined, value);
+	}
+
+	matchRefs(value, refArray) {
+		return this.enforceArray(refArray).includes(value);
+	}
+
+	isArray(arr) {
+		return Array.isArray(arr);
+	}
+
+	hideAttr(node) {
+		return node.setAttribute('hidden', '');
+	}
+
+	prevNode(target) {
+		return node => node.closest(target);
+	}
+
+	hasAttr(attr) {
+		return node => node.hasAttribute(attr);
+	}
+
+	setAttr(entry = {}) {
+		return node => Object.assign(node, entry);
+	}
+
+	isKeyStroke(event) {
+		return !this.matchRef(false, this.isTab(event));
+	}
+
+	canUse(value) {
+		return !this.matchRefs(true, [this.isNull(value), this.isDefined(value)]);
+	}
+
+	removeAttr(attr) {
+		return node => this.hasAttr(attr)(node) && node.removeAttribute(attr);
+	}
+
+	isFocusable(event) {
+		return this.matchRefs(event.target, this.queryFocusableAll()); // focusable elements
+	}
+
+	hideHelpers() {
+		return event => {
+			const helpers = [...this.nodelist.helpers];
+			helpers.forEach(this.setAttr({ hidden: 'hidden' }));
+		};
+	}
+
+	getHelpers() {
+		const radio = [
+			...document.querySelectorAll('[aria-required="true"] .helper-message')
+		];
+		const required = [...document.querySelectorAll('[required]')].map(
+			node => node.parentElement.nextElementSibling
+		);
+		return [...radio, ...required];
+	}
+
+	showHelpers(required = true) {
+		return event => {
+			const helpers = [...this.nodelist.helpers];
+			this.showNodes(helpers)(event);
+		};
+	}
+
+	showNodes(nodelist) {
+		return event => nodelist.forEach(this.removeAttr('hidden'));
+	}
+
+	isObject(obj) {
+		return !this.matchRefs(false, [
+			this.matchRef(undefined, obj),
+			this.matchRef('object', typeof obj),
+			this.matchRef(false, Array.isArray(obj))
+		]);
+	}
+
+	isTab(event) {
+		return !this.matchRefs(false, [
+			this.matchRefs(event.type, 'keyup'), // keyboard in use
+			this.matchRefs(event.keyCode, 9) // tab key pressed
+		]);
+	}
+
+	isEnabled(activator = this.nodelist.activator) {
+		return !this.matchRefs(false, [
+			!this.isNull(activator), // node found
+			this.matchRef(true, activator.checked) // node is "checked"
+		]);
+	}
+
+	isArrows(event) {
+		return !this.matchRefs(false, [
+			this.matchRefs(event.type, 'keyup'), // keyboard in use
+			this.matchRefs(event.keyCode, [37, 38, 39, 40]) // arrow keys pressed
+		]);
+	}
+
+	isRadio(node) {
+		return !this.matchRefs(false, [
+			this.matchRefs(node.tagName, 'INPUT'), // input element
+			this.matchRefs(node.type, 'radio') // radio type
+		]);
+	}
+
+	isCheckbox(node) {
+		return !this.matchRefs(false, [
+			this.matchRefs(node.tagName, 'INPUT'), // input element
+			this.matchRefs(node.type, 'checkbox') // checkbox type
+		]);
+	}
+
+	isApproved(event) {
+		return !this.matchRefs(false, [
+			this.canUse(this.nodelist.flow),
+			this.isEnabled(this.nodelist.activator),
+			this.isTab(event),
+			this.isFocusable(event)
+		]);
+	}
+
+	convertToRem(numerator) {
+		const root = document.documentElement;
+		const font = getComputedStyle(root).fontSize;
+		const denominator = parseFloat(font);
+		return `${numerator / denominator}rem`;
+	}
+
+	enforceArray(value) {
+		return this.matchRef(false, this.isArray(value))
+			? [value]
+			: [...new Set(value)];
+	}
+
+	setObjValues(entries, fn) {
+		return Object.entries(entries).reduce(this.curryReduce(fn), {});
+	}
+
+	curryReduce(fn) {
+		return (cache, [key, value]) => ({
+			...cache,
+			[key]: fn(value)
+		});
+	}
+
+	hideNodes(nodelist) {
+		return event => {
+			nodelist.forEach(this.hideAttr);
+			event.preventDefault(); // TODO: Remove once ready
+		};
+	}
+
+	getPosition(target) {
+		const { top, left, width, height } = target.getBoundingClientRect();
+		return { top, left, width, height };
+	}
+
+	excludeSelectors(exclude = [this.activator]) {
+		return [
+			'[disabled]',
+			'[hidden]',
+			'[aria-hidden="true"]',
+			'[tabindex="-1"]',
+			...exclude
+		]
+			.map(attr => `:not(${attr})`)
+			.join('');
+	}
+
+	includeSelectors(excludes) {
+		return ['button', 'input', 'textarea', 'select', 'details', 'a[href]']
+			.map(name => name + excludes)
+			.join(', ');
+	}
+
+	queryFocusableAll() {
+		const excludes = this.excludeSelectors();
+		const includes = this.includeSelectors(excludes);
+		return [...document.querySelectorAll(includes)];
+	}
+
+	// eventFinished(adjusting) {
+	// 	this.hideAttr(this.nodelist.flow);
+	// 	return event => {
+	// 		window.clearTimeout(adjusting);
+	// 		adjusting = setTimeout(this.moveFlow, 66);
+	// 	};
+	// }
+
+	appendTo() {
+		this.nodelist.app.insertAdjacentHTML(
+			'beforeend',
+			`<div id="focus-flow" role="presentation" />`
+		);
+	}
+
+	conceal(events = this.events.conceal) {
+		const selected = [...document.querySelectorAll('[aria-selected="true"]')];
+		selected.forEach(this.setAttr({ ariaSelected: 'false' }));
+		return event => {
+			this.matchRefs(event.type, events) &&
+				this.nodelist.flow.classList.remove('active');
+			this.matchRef('submit', event.type) && event.preventDefault();
+		};
+	}
+
+	update(selectors) {
+		this.create(selectors);
+		return event => {
+			let target = event.target;
+			const ignore = [this.nodelist.activator, document.body];
+			if (!this.matchRefs(target, ignore) && this.isEnabled()) {
+				target = !this.matchRefs(target.tagName, 'BUTTON')
+					? this.prevNode(this.selectors.inputWrapper)(target)
+					: target;
+				this.conceal([event.type])(event);
+				this.setAttr({ ariaSelected: 'true' })(target);
+				// TODO: const { borderRadius } = getComputedStyle(parent);
+				const style = this.setObjValues(
+					this.getPosition(target),
+					this.convertToRem
+				);
+				const node = this.nodelist.flow;
+				node.classList.add('active');
+				this.setAttr({ ...style })(node.style);
+			}
+		};
+	}
+
+	create(options = {}) {
+		this.selectors = options;
+		const {
+			insertAt,
+			selector,
+			activator,
+			helperText,
+			inputWrapper,
+			radioClass,
+			conceal
+		} = this.selectors;
+		this.events = {
+			conceal
+		};
+		this.nodelist = {
+			app: document.querySelector(insertAt),
+			activator: document.querySelector(activator),
+			flow: document.querySelector(selector),
+			target: document.activeElement,
+			helpers: document.querySelectorAll(helperText),
+			wrappers: document.querySelectorAll(inputWrapper),
+			radios: document.querySelectorAll(radioClass)
+		};
+		this.isNull(this.nodelist.flow) && this.appendTo(this.nodelist.app);
+		return this;
+	}
+
+	observe() {
+		const watch = window.addEventListener;
+		watch('keyup', this.update(this.selectors), false);
+		watch(
+			'submit',
+			event => {
+				this.conceal()(event);
+				this.showHelpers()(event);
+				event.preventDefault();
+			},
+			false
+		);
+		watch('reset', this.hideHelpers(), false);
+		for (const event of this.events.conceal) {
+			watch(event, this.conceal(), false);
+		}
+	}
 }
 
-export default new FocusFlow
+export default new FocusFlow();
